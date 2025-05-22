a9438dd5cefd9cc7d89dc8eb7780171b637fcf10
RuiYuriAfricano <yuriafricano03@gmail.com>
2025-05-22 03:05:45 +0100

    Aumentado limite de tamanho para requisições para 50MB

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +10,9 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-
+// Aumentar limite para 50MB, por exemplo
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
